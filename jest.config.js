40545a3c89a3b538378e534df24c8ac66fecd7a0
Margarets00 <minj01134@gmail.com>
2025-06-02 13:48:57 +0900

    fix: TypeScript build configuration

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,13 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.test.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.test.json'
+    }
+  },
   collectCoverage: true,
   collectCoverageFrom: [
     'src/**/*.ts',
